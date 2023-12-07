df026464258855dacc25dfeed6d71bdd1865c1b7
Subodh <sohonisubodh@gmail.com>
2023-12-07 22:29:39 +0530

    numbers practice

---
+++ week-1.2_offline/level1/02-number.js
@@ -0,0 +1,16 @@
+function exampleparseint(value){
+    console.log("Int value:",parseInt(value));
+
+}
+
+exampleparseint("42");
+exampleparseint("42px");
+exampleparseint("3.14");
+
+function exampleparsefloat(value){
+    console.log("Float value:",parseFloat(value));
+}
+
+exampleparsefloat("3.14");
+exampleparsefloat("42");
+exampleparsefloat("42px");
