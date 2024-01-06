4e01cffbf591729ecd72ff3cb24f4eab1263782e
Subodh <sohonisubodh@gmail.com>
2024-01-06 16:16:39 +0530

    commit

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -6,19 +6,58 @@ mongoose.connect(process.env.MONGO_URI);
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
-    username:String,
-    password:String
-});
+    username: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        length: 8,
+      },
+    },
+    { timestamps: true }
+);
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
-    username:String,
-    password:String
-});
+    username: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        length: 8,
+      },
+      purchasedCourses: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Course",
+        },
+      ],
+    },
+    { timestamps: true });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
-});
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      description: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      imageLink: {
+        type: String,
+        required: true,
+      },
+    });
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
